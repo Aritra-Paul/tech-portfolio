836e051327814f97c1a6bd24b307e7bc6884ab13
Aritra Paul <aritrapaul48.ap@gmail.com>
2020-10-02 15:26:50 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Aritra Paul', // Title for your website.
+    tagline: 'Looking to learn more on how I can help increase diversity in Tech.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://aritra-paul.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'aritra-p-0758a511a',
         twitter: '',
-        github: '',
+        github: 'Aritra-Paul',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
